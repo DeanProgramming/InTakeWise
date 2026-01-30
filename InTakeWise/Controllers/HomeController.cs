ac1ed2dfb98fdbfab7d40259cea4f4a8bc870128
DeanProgramming <110070686+DeanProgramming@users.noreply.github.com>
2026-01-30 21:26:20 +0000

    Initial Push

---
+++ InTakeWise/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using InTakeWise.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace InTakeWise.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly UserManager<IdentityUser> _userManager;
+
+
+        public HomeController(ILogger<HomeController> logger, UserManager<IdentityUser> userManager)
+        {
+            _logger = logger;
+            _userManager = userManager;
+        }
+
+        [AllowAnonymous]
+        public async Task<IActionResult> Index()
+        {
+            if (User.Identity?.IsAuthenticated != true)
+                return RedirectToPage("/Account/Login", new { area = "Identity" });
+
+            var user = await _userManager.GetUserAsync(User);
+
+            return View(new HomeViewModel
+            {
+                UserName = user?.UserName
+            });
+        } 
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ InTakeWise/Models/HomeViewModel.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InTakeWise.Models
+{
+    public class HomeViewModel
+    {
+        public string? UserName { get; set; }
+    }
+}
